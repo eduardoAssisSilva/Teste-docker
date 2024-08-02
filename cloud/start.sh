365e61ef130a8e87a3d65471100e1b4563b1977b
eduardo.silva <j98541@amcontratos.com.br>
2024-08-02 17:22:12 -0300

    Teste Cloud

---
+++ cloud/start.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "Starting the container..."
+# Outros comandos para inicializar
+
+cat ~/catkin_ws/src/fixed_ros_pound_cloud_example/src/libwrapper/config/config.h 
+
+
+echo "------------------------------------------------------------------------------"
+# Replace variables in config.yaml
+cat ~/catkin_ws/src/fixed_ros_pound_cloud_example/src/libwrapper/config/config.yaml
+echo "------------------------------------------------------------------------------"
+
+envsubst < ~/catkin_ws/src/fixed_ros_pound_cloud_example/src/libwrapper/config/config.yaml \
+> ~/catkin_ws/src/fixed_ros_pound_cloud_example/src/libwrapper/config/config.yaml.tmp && \
+mv ~/catkin_ws/src/fixed_ros_pound_cloud_example/src/libwrapper/config/config.yaml.tmp \
+~/catkin_ws/src/fixed_ros_pound_cloud_example/src/libwrapper/config/config.yaml
+
+
+# Manter o container ativo
+exec "$SHELL"
