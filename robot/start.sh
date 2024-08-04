630547266c759e086fce12b5e408270be58fe2f2
eduardo.silva <j98541@amcontratos.com.br>
2024-08-04 09:26:04 -0300

    Delete logs desnecessarios

--- robot/start.sh
+++ robot/start.sh
@@ -2,21 +2,12 @@
 echo "Starting the container..."
 # Outros comandos para inicializar
 
-cat ~/catkin_ws/src/fixed_ros_pound_cloud_example/src/libwrapper/config/config.h 
-
-
-echo "------------------------------------------------------------------------------"
-# Replace variables in config.yaml
-cat ~/catkin_ws/src/fixed_ros_pound_cloud_example/src/libwrapper/config/config.yaml
-echo "------------------------------------------------------------------------------"
-
 envsubst < ~/catkin_ws/src/fixed_ros_pound_cloud_example/src/libwrapper/config/config.yaml \
 > ~/catkin_ws/src/fixed_ros_pound_cloud_example/src/libwrapper/config/config.yaml.tmp && \
 mv ~/catkin_ws/src/fixed_ros_pound_cloud_example/src/libwrapper/config/config.yaml.tmp \
 ~/catkin_ws/src/fixed_ros_pound_cloud_example/src/libwrapper/config/config.yaml
 
-chmod +x config.yaml
-
+chmod +x ~/catkin_ws/src/fixed_ros_pound_cloud_example/src/libwrapper/config/config.yaml
 
 # Manter o container ativo
 exec "$SHELL"
